f279bcd54b32cd467ae0baeb5987051bd85fcf83
mikity <78365241+KakinumaMiki@users.noreply.github.com>
2021-05-05 20:21:23 +0900

    投稿表示部分実装中

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,22 @@
 var express = require('express');
+const Post = require('../models/post');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('posts', { h1: '秘密の匿名掲示板' });
+router.get('/', function (req, res, next) {
+
+  Post.findAll({
+    order: [['id', 'DESC']]
+  }).then((posts) => {
+    if (posts) {
+      console.log('POST:' + posts);
+      res.render('posts', { posts: posts });
+    // } else {
+    //   const err = new Error('ERROR');
+    //   err.status = 404;
+    //   next(err);
+    }
+  })
 });
 
 module.exports = router;
