512e9fe11dcd3a45ca60f2ecf6741540d02d645f
mikity <78365241+KakinumaMiki@users.noreply.github.com>
2021-05-05 22:40:36 +0900

    タイトル,本文,投稿時間の投稿の実装

--- routes/posts.js
+++ routes/posts.js
@@ -1,22 +1,45 @@
 var express = require('express');
 const Post = require('../models/post');
 var router = express.Router();
+const moment = require('moment-timezone');
 
-/* GET home page. */
+/* GET */
 router.get('/', function (req, res, next) {
-
   Post.findAll({
     order: [['id', 'DESC']]
   }).then((posts) => {
-    if (posts) {
-      console.log('POST:' + posts);
-      res.render('posts', { posts: posts });
-    // } else {
-    //   const err = new Error('ERROR');
-    //   err.status = 404;
-    //   next(err);
-    }
+    posts.forEach((post) => {
+      post.content = post.content.replace(/\+/g, ' ');
+      post.formattedCreatedAt = moment(post.createdAt).tz('Asia/Tokyo').format('YYYY年MM月DD日 HH時mm分ss秒');
+    });
+    console.log('POST:' + posts);
+    res.render('posts', { posts: posts });
   })
+});
+
+router.post('/', function (req, res, next) {
+
+  console.log('title: ' + req.body.title);
+  console.log('content: ' + req.body.content);
+  // console.log('postedBy: ' + req.body);
+
+
+
+
+
+  Post.create({
+
+
+    title: req.body.title,
+    content: req.body.content,
+    // postedBy: req.user.id
+
+
+  }).then((post) => {
+
+    res.redirect('/posts');
+  });
+
 });
 
 module.exports = router;
