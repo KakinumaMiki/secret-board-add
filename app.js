514539972d9e0331018292b1cf776d6e0ee9f9f0
mikity <78365241+KakinumaMiki@users.noreply.github.com>
2021-05-06 20:13:09 +0900

    Github認証の追加

--- app.js
+++ app.js
@@ -4,12 +4,40 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var helmet = require('helmet');
+var session = require('express-session');
+var passport = require('passport');
+
+var GitHubStrategy = require('passport-github2').Strategy;
+var GITHUB_CLIENT_ID = '442738d819ad6997995a';
+var GITHUB_CLIENT_SECRET = '3171eea68d44d5de15630417e55cf4a2a5d3dbe7';
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+  done(null, obj);
+});
+
+
+passport.use(new GitHubStrategy({
+  clientID: GITHUB_CLIENT_ID,
+  clientSecret: GITHUB_CLIENT_SECRET,
+  callbackURL: 'http://localhost:8000/auth/github/callback'
+},
+  function (accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+      return done(null, profile);
+    });
+  }
+));
 
 // ルーター読み込み
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var postsRouter = require('./routes/posts');
 var profileRouter = require('./routes/profile');
+var loginRouter = require('./routes/login');
+var logoutRouter = require('./routes/logout');
 
 // モデル読み込み
 var User = require('./models/user');
@@ -32,10 +60,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({ secret: 'e55be81b307c1c09', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
 app.use('/profile', profileRouter);
+app.use('/login', loginRouter);
+app.use('/logout', logoutRouter);
+
+app.get('/auth/github',
+  passport.authenticate('github', { scope: ['user:email'] }),
+  function (req, res) {
+});
+
+app.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function (req, res) {
+    res.redirect('/');
+});
 
 
 
