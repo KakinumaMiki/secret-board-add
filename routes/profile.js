8fc5098aa7d1f36bc631ef121e910d1e973db097
mikity <78365241+KakinumaMiki@users.noreply.github.com>
2021-05-06 20:41:50 +0900

    '/'の編集、profile画面のユーザー名の表示

--- routes/profile.js
+++ routes/profile.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('profile', { h1: '秘密の匿名掲示板' });
+  res.render('profile', { h1: '秘密の匿名掲示板', user: req.user });
 });
 
 module.exports = router;
