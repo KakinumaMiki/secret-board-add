4e485bb742bd88b9b4c4fe0b41ec583091d78334
mikity <78365241+KakinumaMiki@users.noreply.github.com>
2021-05-07 15:28:23 +0900

    postedByとuser.idの比較完了

--- routes/posts.js
+++ routes/posts.js
@@ -1,26 +1,36 @@
 var express = require('express');
 const Post = require('../models/post');
+const User = require('../models/user');
 var router = express.Router();
 const moment = require('moment-timezone');
 
 /* GET */
 router.get('/', function (req, res, next) {
+  let storedPost = null;
+  let name = null;
   Post.findAll({
     order: [['id', 'DESC']]
   }).then((posts) => {
     posts.forEach((post) => {
       post.content = post.content.replace(/\+/g, ' ');
       post.formattedCreatedAt = moment(post.createdAt).tz('Asia/Tokyo').format('YYYY年MM月DD日 HH時mm分ss秒');
+      name = post.postedBy;
+      console.log('user(name):' + name);
       console.log('user(post):' + post.postedBy);
+      User.findOne({
+        where: { userId: name}
+      }).then((user) => {
+        console.log(user.username);
+      })
+      res.render('posts',
+      {
+        posts: posts,
+        user: req.user,
+        // user: user.username,
+        h1: '秘密の匿名掲示板'
+      });
+    })
     });
-    console.log('user(posts):' + posts.postedBy);
-    res.render('posts',
-    {
-      posts: posts,
-      user: req.user,
-      h1: '秘密の匿名掲示板'
-    });
-  })
 });
 
 router.post('/', function (req, res, next) {
